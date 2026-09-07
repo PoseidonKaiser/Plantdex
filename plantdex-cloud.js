7805b0c21d25455c9282a928d1cd15d83aa67627
PoseidonKaiser <ceasarromero@gmail.com>
2026-09-07 15:01:01 -0500

    Bust cache for Plantdex egress fix

--- plantdex-cloud.js
+++ plantdex-cloud.js
@@ -1,9 +1,9 @@
 (function(){
   if(document.readyState==='loading'){
-    document.write('<scr'+'ipt src="plantdex-cloud-v2.js?v=20260828-mobile-savefix1"></scr'+'ipt><scr'+'ipt src="plantdex-pedigree.js?v=20260828-pedigree1"></scr'+'ipt>');
+    document.write('<scr'+'ipt src="plantdex-cloud-v2.js?v=20260907-egressfix1"></scr'+'ipt><scr'+'ipt src="plantdex-pedigree.js?v=20260828-pedigree1"></scr'+'ipt>');
   }else{
     const cloud=document.createElement('script');
-    cloud.src='plantdex-cloud-v2.js?v=20260828-mobile-savefix1';
+    cloud.src='plantdex-cloud-v2.js?v=20260907-egressfix1';
     cloud.onload=()=>{
       const pedigree=document.createElement('script');
       pedigree.src='plantdex-pedigree.js?v=20260828-pedigree1';
